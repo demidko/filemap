e9ab9a357520e49b7c15b7d55c3a06963192ec30
Daniil Demidko <demidko@farpost.com>
2021-07-11 09:28:21 +1000

    fmt -> spdlog

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,5 @@
 #include <tgbot/tgbot.h>
-#include <fmt/core.h>
-#include <fmt/color.h>
+#include <spdlog/spdlog.h>
 
 int main() {
   std::unordered_map<int32_t, std::string> map;
@@ -11,11 +10,11 @@ int main() {
     }
     if (map.find(m->from->id) != map.cend()) {
       bot.getApi().sendMessage(m->chat->id, map[m->from->id], false, m->messageId);
-      fmt::print(fg(fmt::color::dark_olive_green), "user {} requested file {}\n", m->from->id, map[m->from->id]);
+      spdlog::info("user {} requested file {}\n", m->from->id, map[m->from->id]);
     }
     if (m->document) {
       map[m->from->id] = m->document->fileId;
-      fmt::print(fg(fmt::color::dark_blue), "user {} saved file {}", m->from->id, m->document->fileId);
+      spdlog::info("user {} saved file {}", m->from->id, m->document->fileId);
     }
   });
   for (TgBot::TgLongPoll p(bot);; p.start());
