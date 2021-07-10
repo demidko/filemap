3cd1da9d21926bea0812e0a11401e68a12692be5
Daniil Demidko <demidko@farpost.com>
2021-07-11 08:52:38 +1000

    colors in terminal

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,11 +11,11 @@ int main() {
     }
     if (map.find(m->from->id) != map.cend()) {
       bot.getApi().sendMessage(m->chat->id, map[m->from->id], false, m->messageId);
-      fmt::print(fg(fmt::color::dark_olive_green), "{}\n", m->from->id);
+      fmt::print(fg(fmt::color::dark_olive_green), "user {} requested file {}\n", m->from->id, map[m->from->id]);
     }
     if (m->document) {
       map[m->from->id] = m->document->fileId;
-      fmt::print(fg(fmt::color::dark_blue), "[{}] = {}", m->from->id, m->document->fileId);
+      fmt::print(fg(fmt::color::dark_blue), "user {} saved file {}", m->from->id, m->document->fileId);
     }
   });
   for (TgBot::TgLongPoll p(bot);; p.start());
