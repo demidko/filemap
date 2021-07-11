2d62a4fe26edb4d5c8f15243c2f0cbe140b95b12
Daniil Demidko <demidko@farpost.com>
2021-07-11 10:07:03 +1000

    upd

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,11 +10,11 @@ int main() {
     }
     if (map.find(m->from->id) != map.cend()) {
       bot.getApi().sendMessage(m->chat->id, map[m->from->id], false, m->messageId);
-      spdlog::info("user {} requested file {}\n", m->from->id, map[m->from->id]);
+      spdlog::info("{} requested {}", m->from->id, map[m->from->id]);
     }
     if (m->document) {
       map[m->from->id] = m->document->fileId;
-      spdlog::info("user {} saved file {}", m->from->id, m->document->fileId);
+      spdlog::info("{} saved {}", m->from->id, m->document->fileId);
     }
   });
   for (TgBot::TgLongPoll p(bot);; p.start());
