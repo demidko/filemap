6f8bb9eb366654df49365fdb8353e5bec6f87369
Daniil Demidko <demidko@farpost.com>
2021-07-13 03:39:24 +1000

    k-v support

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-### [`@filemapbot`](https://t.me/filemapbot) `= f(`[`message`](https://core.telegram.org/bots/api#message)`) →` [`your previous document id`](https://core.telegram.org/bots/api#document)
+# Filemapbot
 
-### [![Deploy to DigitalOcean](https://www.deploytodo.com/do-btn-blue-ghost.svg)](https://cloud.digitalocean.com/apps/new?repo=https://github.com/demidko/filemapbot/tree/main)
+This is a key-value database for telegram bots.
+
+1. To enable bots to interact with each other, add them to the same channel with administrator rights.
+1. To save the file, send it as a text message attachment. This text will be key.
+1. To get the [file ID](https://core.telegram.org/bots/api#document), send the key in a text message.
+
+## [![Deploy to DigitalOcean](https://www.deploytodo.com/do-btn-blue-ghost.svg)](https://cloud.digitalocean.com/apps/new?repo=https://github.com/demidko/filemapbot/tree/main)
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -2,19 +2,21 @@
 #include <spdlog/spdlog.h>
 
 int main() {
-  std::unordered_map<int32_t, std::string> map;
+  std::unordered_map<int32_t, std::unordered_map<std::string, std::string>> map;
   TgBot::Bot bot(getenv("TOKEN"));
-  bot.getEvents().onAnyMessage([&](auto &&m) {
-    if (!m->from->id) {
+  bot.getEvents().onAnyMessage([&](const TgBot::Message::Ptr &m) {
+    if (m->text.empty() || !m->from->id) {
+      return;
+    }
+    if (m->document) {
+      map[m->from->id][m->text] = m->document->fileId;
+      spdlog::info("{} saved {} as {}", m->from->id, m->document->fileId, m->text);
       return;
     }
     if (map.find(m->from->id) != map.cend()) {
-      bot.getApi().sendMessage(m->chat->id, map[m->from->id], false, m->messageId);
-      spdlog::info("{} requested {}", m->from->id, map[m->from->id]);
-    }
-    if (m->document) {
-      map[m->from->id] = m->document->fileId;
-      spdlog::info("{} saved {}", m->from->id, m->document->fileId);
+      auto value = map[m->from->id][m->text];
+      bot.getApi().sendMessage(m->chat->id, value, false, m->messageId);
+      spdlog::info("{} requested {} by {}", m->from->id, value, m->text);
     }
   });
   for (TgBot::TgLongPoll p(bot);; p.start());
